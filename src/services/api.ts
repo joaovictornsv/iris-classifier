94d998773c0031b4ce4955592900a493bdbd76a0
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-03-22 22:03:10 -0300

    refactor: adjust api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,19 @@
-import axios from 'axios'
+/* eslint-disable camelcase */
+import axios, { AxiosResponse } from 'axios'
+
+interface IRequest {
+  sepal_length: number;
+  sepal_width: number;
+  petal_length: number;
+  petal_width: number;
+}
 
 const api = axios.create({
   baseURL: 'https://iris-api-classifier.herokuapp.com'
 })
+
+export const predictClass = async (requestBody: IRequest): Promise<AxiosResponse> => {
+  return await api.post('/api/predict', requestBody)
+}
 
 export default api
